ed7d74df979521c3627fcb0256be92bad3210e5b
alexsuri <79443136+alexsuri@users.noreply.github.com>
2021-04-24 23:13:35 +0700

    inital ver
    inital ver

---
+++ test_ProcSonar1.sql
@@ -0,0 +1,27 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[test_ProcSonar1]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	BEGIN TRANSACTION
+	BEGIN TRY
+		SELECT 1
+	END TRY
+	BEGIN CATCH
+		ROLLBACK;
+	END CATCH
+
+END
+GO
